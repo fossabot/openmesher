745fcf81762aa8306aae09130331cf2e79cab37c
Aaron C. de Bruyn <aaron@heyaaron.com>
2016-12-03 11:28:38 -0800

    PEP8

--- OpenMesher/tunnelobjects/__init__.py
+++ OpenMesher/tunnelobjects/__init__.py
@@ -1,4 +1,3 @@
-import IPy
 import ipaddr
 import probstat
 import os
@@ -21,10 +20,10 @@ class Router():
             self.hostname = name
 
     def __unicode__(self):
-        return '%s' %(self.fqdn or self.hostname)
+        return '%s' % (self.fqdn or self.hostname)
 
     def __str__(self):
-        return '%s' %(self.fqdn or self.hostname)
+        return '%s' % (self.fqdn or self.hostname)
 
 
 class Link():
@@ -40,21 +39,25 @@ class Link():
         if self.server is None and self.client is None:
             raise Exception('Trying to retrieve link name of unnamed link')
         else:
-            return '%s-%s' %(self.server.hostname, self.client.hostname)
+            return '%s-%s' % (self.server.hostname, self.client.hostname)
 
     def _genkey(self):
-        proc = subprocess.Popen("%s --genkey --secret /dev/stdout" %(self.OpenVPNPath), shell=True, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(
+            '%s --genkey --secret /dev/stdout' % (self.OpenVPNPath),
+            shell=True, stdout=subprocess.PIPE
+        )
         proc.wait()
         (stdout, stderr) = proc.communicate()
         if proc.returncode != 0:
-            raise Exception('OpenVPN key gen failed for %s' %(self.linkname()))
+            raise Exception('OpenVPN key gen failed for %s' % (self.linkname()))
         self.key = stdout
 
     def __init__(self, serverrouter, clientrouter, linkport, iface_number, block):
-        #BUG: Hack because Ubuntu 11.10 moved openvpn from /bin to /sbin and the user doesn't have a path to /sbin.  Need to search path, then check /sbin
+        # BUG: Hack because Ubuntu 11.10 moved openvpn from /bin to /sbin and the user doesn't have a path to /sbin.
+        # Need to search path, then check /sbin
         for ovp in ['/bin', '/sbin', '/usr/bin', '/usr/sbin']:
-            if os.path.isfile('%s/openvpn' %ovp):
-                self.OpenVPNPath = "%s/openvpn" %(ovp)
+            if os.path.isfile('%s/openvpn' % (ovp)):
+                self.OpenVPNPath = "%s/openvpn" % (ovp)
                 break
 
         if not self.OpenVPNPath:
@@ -64,17 +67,29 @@ class Link():
         self.server = serverrouter
         self.client = clientrouter
         self.port = linkport
-        self.iface = 'tun%s' %(iface_number)
-        self.server.interfaces.append('tun%s' %(iface_number))
-        self.client.interfaces.append('tun%s' %(iface_number))
+        self.iface = 'tun%s' % (iface_number)
+        self.server.interfaces.append('tun%s' % (iface_number))
+        self.client.interfaces.append('tun%s' % (iface_number))
         if self.key is None:
             self._genkey()
 
     def __unicode__(self):
-        return 'Server: %s, Client: %s, Port: %s, Iface: %s Block: %s' %(self.server.fqdn, self.client.fqdn, self.port, self.iface, self.block)
+        return 'Server: %s, Client: %s, Port: %s, Iface: %s Block: %s' % (
+            self.server.fqdn,
+            self.client.fqdn,
+            self.port,
+            self.iface,
+            self.block,
+        )
 
     def __str__(self):
-        return 'Server: %s, Client: %s, Port: %s, Iface: %s Block: %s' %(self.server.fqdn, self.client.fqdn, self.port, self.iface, self.block)
+        return 'Server: %s, Client: %s, Port: %s, Iface: %s Block: %s' % (
+            self.server.fqdn,
+            self.client.fqdn,
+            self.port,
+            self.iface,
+            self.block,
+        )
 
     def isServer(self, routerfqdn):
         return routerfqdn == self.server.fqdn
@@ -88,32 +103,40 @@ class Mesh():
     subnets = []
 
     def __init__(self, routerlinks, ports, subnets):
-        logging.debug('Subnets available: %s' %(subnets))
+        logging.debug('Ports available: %s' % (len(ports)))
+        logging.debug('Subnets available: %s' % (subnets))
         for sub in subnets:
-            logging.debug('Processing subnet: %s' %(sub))
+            logging.debug('Processing subnet: %s' % (sub))
             blocks = ipaddr.IPNetwork(sub, strict=True).subnet(new_prefix=30)
             for block in blocks:
-                logging.debug('P2P block available: %s' %(block))
+                logging.debug('P2P block available: %s' % (block))
                 self.subnets.append(block)
 
-        logging.debug('Loaded %s /30s' %(len(self.subnets)))
+        logging.debug('Loaded %s /30s' % (len(self.subnets)))
         self.subnets.reverse()
 
         links_needed = None
-        #Create router objects for each router and client
+        # Create router objects for each router and client
         for rtr in routerlinks:
-            logging.debug('Creating router object: %s' %(rtr))
+            logging.debug('Creating router object: %s' % (rtr))
             self.routers[rtr] = Router(rtr)
             for rtrcli in routerlinks[rtr]:
-                logging.debug('Creating router (client): %s' %(rtrcli))
+                logging.debug('Creating router (client): %s' % (rtrcli))
                 self.routers[rtrcli] = Router(rtrcli)
 
-        #For each router, create a link object, assign a server and client router object, assign ports and interface numbers along with a subnet.
+        # For each router, create a link object, assign a server and client router object, assign ports and interface
+        # numbers along with a subnet.
         for rtr in routerlinks:
             for rtrclient in routerlinks[rtr]:
-                logging.debug('Creating new link from %s to %s with iface %s' %(rtr, rtrclient, self.iface_count))
+                logging.debug('Creating new link from %s to %s with iface %s' % (rtr, rtrclient, self.iface_count))
                 try:
-                    newlink = Link(self.routers[rtr], self.routers[rtrclient], ports.pop(), self.iface_count, self.subnets.pop())
+                    newlink = Link(
+                        self.routers[rtr],
+                        self.routers[rtrclient],
+                        ports.pop(),
+                        self.iface_count,
+                        self.subnets.pop()
+                    )
                 except IndexError as e:
                     raise IndexError('Not enough ports available.  Add additional port ranges and try again.')
 
@@ -121,7 +144,7 @@ class Mesh():
                     self.links[rtr] = []
                 self.links[rtr].append(newlink)
 
-                if not self.links.has_key(rtrclient):
+                if rtrclient not in self.links:
                     self.links[rtrclient] = []
                 self.links[rtrclient].append(newlink)
                 self.iface_count += 1
@@ -129,19 +152,19 @@ class Mesh():
         links_needed = 0
         for srv in self.links:
             links_needed += len(self.links[srv])
-        logging.debug('%s links needed' %(links_needed))
+        logging.debug('%s links needed' % (links_needed))
 
         subnets_available = len(self.subnets)
-        logging.debug('%s subnets available' %(subnets_available))
+        logging.debug('%s subnets available' % (subnets_available))
 
         if links_needed > subnets_available:
-            raise Exception('Not enough subnets available: %s needed, %s available' %(links_needed, subnets_available))
+            raise Exception('Not enough subnets available: %s needed, %s available' % (links_needed, subnets_available))
 
     def __unicode__(self):
-        return '%s routers, %s links' %(len(self.routers), len(probstat.Combination(self.routers.keys(), 2)))
+        return '%s routers, %s links' % (len(self.routers), len(probstat.Combination(self.routers.keys(), 2)))
 
     def __str__(self):
-        return '%s routers, %s links' %(len(self.routers), len(probstat.Combination(self.routers.keys(), 2)))
+        return '%s routers, %s links' % (len(self.routers), len(probstat.Combination(self.routers.keys(), 2)))
 
     def get_server_links(self, sRouter):
         rLinks = []
@@ -156,4 +179,3 @@ class Mesh():
             if link.client.name == sRouter:
                 rLinks.append(link)
         return rLinks
-
